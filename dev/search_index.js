e0f0cceb8bff76ebb4f171ff0f11598afd2f2756
Documenter.jl <documenter@juliadocs.github.io>
2021-10-20 18:21:16 +0000

    build based on 44c5b02

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageVT","category":"page"},{"location":"#DemoPackageVT","page":"Home","title":"DemoPackageVT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageVT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageVT]","category":"page"}]
+}
